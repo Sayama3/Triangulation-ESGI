e6432c18711eb8ce86c883bb4e497567203f42e2
Iannis Poquet <ianpo@live.fr>
2025-01-26 15:46:24 +0100

    Discover tests

--- Tests/src/test_math.cpp
+++ Tests/src/test_math.cpp
@@ -12,5 +12,5 @@ TEST(HelloTest, BasicAssertions) {
 	// Expect two strings not to be equal.
 	EXPECT_STRNE("hello", "world");
 	// Expect equality.
-	EXPECT_EQ(7_r * 6_r, 42.0_r);
+	EXPECT_FLOAT_EQ(7_r * 6_r, 42.0_r);
 }
