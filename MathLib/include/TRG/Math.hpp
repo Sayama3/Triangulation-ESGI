5255b920b39eb1b1db7cc5226ad69c3861b33c5e
Iannis Poquet <ianpo@live.fr>
2025-01-26 16:18:23 +0100

    Re-enable cache + Add some Math operations

--- MathLib/include/TRG/Math.hpp
+++ MathLib/include/TRG/Math.hpp
@@ -3,6 +3,22 @@
 //
 
 #pragma once
+#ifndef GLM_ENABLE_EXPERIMENTAL
+#define GLM_ENABLE_EXPERIMENTAL
+#endif
+
+#include <glm/glm.hpp>
+#include <glm/matrix.hpp>
+#include <glm/vector_relational.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/hash.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#define TRG_SQR(a) (a * a)
 
 namespace TRG::Math {
 
@@ -44,10 +60,63 @@ namespace TRG::Math {
 	using Real = float;
 #endif
 
+	typedef glm::qua<Real> Quat;
+	typedef glm::vec<4, Real> Vec4;
+	typedef glm::vec<3, Real> Vec3;
+	typedef glm::vec<2, Real> Vec2;
+	typedef glm::vec<1, Real> Vec1;
+
+	typedef glm::mat<4, 4, Real> Mat4x4; typedef Mat4x4 Mat4;
+	typedef glm::mat<3, 3, Real> Mat3x3; typedef Mat3x3 Mat3;
+	typedef glm::mat<2, 2, Real> Mat2x2; typedef Mat2x2 Mat2;
+	typedef glm::mat<3, 2, Real> Mat3x2;
+
 	namespace Literal
 	{
 		constexpr Real operator ""_r(const unsigned long long int value) {return static_cast<Real>(value);}
 		constexpr Real operator ""_r(const long double value) {return static_cast<Real>(value);}
+	}
+
+	template<glm::length_t L, typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static glm::mat<L,L,T,Q> Inverse(const glm::mat<L,L,T,Q>& mat) {
+		return glm::inverse(mat);
+	}
+
+	template<glm::length_t L, typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static T Determinant(const glm::mat<L,L,T,Q>& mat) {
+		return glm::determinant(mat);
+	}
+
+	template<glm::length_t L, typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static T Dot(const glm::vec<L,T,Q>& x, const glm::vec<L,T,Q>& y) {
+		return glm::dot(x,y);
+	}
+
+	template<typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static T Cross(const glm::vec<3,T,Q>& x, const glm::vec<3,T,Q>& y) {
+		return glm::cross(x,y);
+	}
+
+	template<glm::length_t L, typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static T Magnitude2(const glm::vec<L,T,Q>& x) {
+		return glm::length2(x);
+	}
+
+	template<glm::length_t L, typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static T Magnitude(const glm::vec<L,T,Q>& x) {
+		return glm::length(x);
+	}
+
+	template<class fwd_iterator, glm::length_t L, typename T = Real, glm::qualifier Q = glm::qualifier::defaultp>
+	inline static glm::vec<L,T,Q> CalculateCenter(const fwd_iterator begin, const fwd_iterator end) {
+		glm::vec<L,T,Q> center{static_cast<T>(0)};
+		uint64_t count{0ull};
+		for (auto it = begin; it != end; ++it) {
+			center += *it;
+			++count;
+		}
+		center /= static_cast<T>(count);
+		return center;
 	}
 
 } // TRG
--- Tests/src/test_math.cpp
+++ Tests/src/test_math.cpp
@@ -7,10 +7,49 @@
 
 using namespace TRG::Math::Literal;
 
+#ifdef TRG_DOUBLE
+	#define EXPECT_REAL_EQ(val1, val2) EXPECT_DOUBLE_EQ(val1, val2)
+	#define ASSERT_REAL_EQ(val1, val2) ASSERT_DOUBLE_EQ(val1, val2)
+#else
+	#define EXPECT_REAL_EQ(val1, val2) EXPECT_FLOAT_EQ(val1, val2)
+	#define ASSERT_REAL_EQ(val1, val2) ASSERT_FLOAT_EQ(val1, val2)
+#endif
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
 	// Expect two strings not to be equal.
 	EXPECT_STRNE("hello", "world");
 	// Expect equality.
-	EXPECT_FLOAT_EQ(7_r * 6_r, 42.0_r);
+	EXPECT_REAL_EQ(7_r * 6_r, 42.0_r);
+}
+
+// Demonstrate some basic assertions.
+TEST(MathTest, BasicVectorTests) {
+	typedef std::array<TRG::Math::Vec2, 4> MathArray;
+	typedef std::vector<TRG::Math::Vec2> MathVector;
+
+	constexpr MathArray arr {
+		TRG::Math::Vec2{-1_r,-1_r},
+		TRG::Math::Vec2{-1_r,+1_r},
+		TRG::Math::Vec2{+1_r,+1_r},
+		TRG::Math::Vec2{+1_r,-1_r},
+	};
+
+	const TRG::Math::Vec2 center = TRG::Math::CalculateCenter<MathArray::const_iterator, 2>(arr.cbegin(),arr.cend());
+
+	EXPECT_REAL_EQ(center.x, 0_r);
+	EXPECT_REAL_EQ(center.y, 0_r);
+
+	// ============================================================== //
+
+	const MathVector arr2 {
+		TRG::Math::Vec2{+0_r,+0_r},
+		TRG::Math::Vec2{+0_r,+3_r},
+		TRG::Math::Vec2{+4_r,+3_r},
+		TRG::Math::Vec2{+4_r,+0_r},
+	};
+	const TRG::Math::Vec2 center2 = TRG::Math::CalculateCenter<MathVector::const_iterator, 2>(arr2.cbegin(),arr2.cend());
+
+	EXPECT_REAL_EQ(center2.x, 2_r);
+	EXPECT_REAL_EQ(center2.y, 3_r/2_r);
 }
